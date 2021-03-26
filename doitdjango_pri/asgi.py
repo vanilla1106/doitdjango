a16aa143f3d110664a2908da051b4b51f913e7c4
unknown <vanilla1106>
2021-03-26 13:19:05 +0900

    Django 프로젝트 생성

---
+++ doitdjango_pri/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for doitdjango_pri project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'doitdjango_pri.settings')
+
+application = get_asgi_application()
